0cfb95f13e35e17b8eda0dab294a7fb7ab62a5b3
Alex Canales <a.canales@laposte.net>
2016-01-06 13:47:19 -0500

    Add the class dashboard which communicates to the tool.

---
+++ src/dashboard.cpp
@@ -0,0 +1,119 @@
+#include <string>
+#include <curl/curl.h>
+#include <rapidjson/document.h>
+
+#include "dashboard.h"
+
+void Dashboard::initialize(std::string baseURL,
+        void (*callbackGetPosition)(bool, float, float, float))
+{
+    //TODO: open connection for test
+    baseURL = baseURL;
+    callback = callbackGetPosition;
+    curl_global_init(CURL_GLOBAL_ALL);  //In windows, init the winsock stuff
+    curl = curl_easy_init();
+
+    headers = NULL;
+    curl_slist_append(headers, "Accept: application/json");
+    curl_slist_append(headers, "Content-Type: application/json");
+    curl_slist_append(headers, "Charsets: UTF-8");
+
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcrp/0.1");
+}
+
+void Dashboard::cleanAll()
+{
+    //TODO: close connection
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(curl);  //Not thread safe!!!
+    curl_global_cleanup();
+}
+
+void Dashboard::setBaseURL(std::string url)
+{
+    baseURL = url;
+}
+
+// Callback must be: void getPosition(bool status, float x, float y, float z);
+void Dashboard::setCallback(void (*callbackGetPosition)(bool, float, float, float))
+{
+    callback = callbackGetPosition;
+}
+
+size_t Dashboard::dataParser(char* buf, size_t size, size_t nmemb, void* up)
+{
+    float x = 0, y = 0, z = 0;
+    std::string data;
+
+    if(callback == NULL)
+        return 0;
+
+    for(int c = 0; c < size*nmemb; c++)
+    {
+        data.push_back(buf[c]);
+    }
+
+    if(buf == NULL)
+        callback(false, x, y, z);
+
+    rapidjson::Document document;
+    document.Parse(buf);
+
+    if(!document.IsObject())
+        callback(false, x, y, z);
+
+    if(!document.HasMember("data") || !document["data"].IsObject())
+        callback(false, x, y, z);
+
+    if(!document["data"].HasMember("status") ||
+            !document["data"]["status"].IsObject())
+    {
+        callback(false, x, y, z);
+    }
+
+    if(!document["data"]["status"].HasMember("posx") &&
+       !document["data"]["status"].HasMember("posy") &&
+       !document["data"]["status"].HasMember("posz"))
+    {
+        callback(false, x, y, z);
+    }
+
+    x = document["data"]["status"]["posx"].GetDouble();
+    y = document["data"]["status"]["posy"].GetDouble();
+    z = document["data"]["status"]["posz"].GetDouble();
+
+    callback(true, x, y, z);
+    return size*nmemb; //tell curl how many bytes we handled
+}
+
+bool Dashboard::getPosition()
+{
+    std::string url = baseURL;
+    url.append("/status");
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, 0);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &dataParser);
+
+    return (curl_easy_perform(curl) == CURLE_OK);
+}
+
+bool Dashboard::setPosition(float x, float y, float z)
+{
+    CURLcode res;
+    std::string url = baseURL;
+    url.append("/code");
+
+    char postField[300];  //TODO: see if enough
+    sprintf(postField, "runtime=g&cmd=G0X%fY%fZ%f", x, y, z);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postField);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, 0);
+
+    res = curl_easy_perform(curl);
+    return res == CURLE_OK;
+}
---
+++ src/dashboard.h
@@ -0,0 +1,33 @@
+#ifndef _DASHBOARD_H_
+#define _DASHBOARD_H_
+
+#include <string>
+#include <curl/curl.h>
+
+/**
+ * Used to communicate to the machine.
+ * Because of the callback functions, everything is static. Plus this class
+ * is unique in this program.
+ */
+class Dashboard
+{
+    static std::string baseURL;
+    static CURL *curl;
+    static struct curl_slist *headers;
+    static void (*callback)(bool, float, float, float);
+    // Parsing the data received on the URL /status
+    static size_t dataParser(char* buf, size_t size, size_t nmemb, void* up);
+public:
+    Dashboard();
+    Dashboard(std::string baseURL);
+    ~Dashboard();
+    static void initialize(std::string baseURL,
+            void (*callbackGetPosition)(bool, float, float, float));
+    static void cleanAll();
+    static void setBaseURL(std::string url);  //xxx.xxx.xxx.xxx:xxxx
+    static void setCallback(void (*callbackGetPosition)(bool, float, float, float));
+    static bool getPosition();
+    static bool setPosition(float x, float y, float z);
+};
+
+#endif
